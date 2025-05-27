0a718b878eee09388d0ac954183db814810a591c
sister-moon <149580595+sister-moon@users.noreply.github.com>
2025-05-27 15:41:56 +0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-  build: {
-    rollupOptions: {
-      output: {
-        format: "iife",
-        entryFileNames: "bundle.js",
-      },
-    },
-  },
+  base: "/",
+
   test: {
     globals: true,
     environment: "happy-dom",
