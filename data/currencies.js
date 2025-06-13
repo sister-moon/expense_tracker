047b1c6edbdcd80d5f8d1fc483179c32c5cf6ad3
sister-moon <149580595+sister-moon@users.noreply.github.com>
2025-06-13 10:14:04 +0500

    Delete data directory

--- data/currencies.js
+++
@@ -1,9 +0,0 @@
-export const currencies = [
-  { code: "RUB", name: "Российский рубль", flag: "🇷🇺" },
-  { code: "USD", name: "Доллар США", flag: "🇺🇸" },
-  { code: "EUR", name: "Евро", flag: "🇪🇺" },
-  { code: "CNY", name: "Китайский юань", flag: "🇨🇳" },
-  { code: "INR", name: "Индийская рупия", flag: "🇮🇳" },
-  { code: "KZT", name: "Казахстанский тенге", flag: "🇰🇿" },
-  { code: "UZS", name: "Узбекский сум", flag: "🇺🇿" },
-];
